688a90f16ebab2ccf57b6363fca8a5c6dec548a6
Peter Matthias Muller <98629178+NippyB@users.noreply.github.com>
2022-08-17 21:52:38 +0800

    Update config.js

--- config.js
+++ config.js
@@ -30,14 +30,15 @@ var config = {
      * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
      */
     imgs: {
-        "心爱的小可爱": "./imgs/xiaokeai.png",
+        "可爱的小程越": "./imgs/xiaokeai.jpg",
+        "生日快乐！": "./imgs/图片17.png",
         // "今天是你的生日": "./imgs/birthday.jpg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
         turn_on: "开始",
-        play: "音乐",
-        bannar_coming: "颜色",
+        play: "Music",
+        bannar_coming: "嗨~",
         balloons_flying: "好像少点东西",
         cake_fadein: "蛋糕？",
         light_candle: "蜡烛？",
